1021feb761bb967b7b1f0a48911a9578a9180453
BryanValc <epicspartanryan@gmail.com>
2026-09-26 01:43:03 +0000

    Add Gut for Unit testing and link to plug.gd github

--- client/sample-game/plug.gd
+++ client/sample-game/plug.gd
@@ -1,5 +1,10 @@
 extends "res://addons/gd-plug/plug.gd"
 
+# More info: https://github.com/imjp94/gd-plug
+
 func _plugging():
 	# Aseprite Wizard (Version v9.8.0-4)
 	plug("viniciusgerevini/godot-aseprite-wizard", {"tag": "v9.8.0-4"})
+	
+	# Unit test
+	plug("bitwes/Gut", {"tag": "v9.7.1"})
