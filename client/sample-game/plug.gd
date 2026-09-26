04cfdb995add4f575372f5f19ac15ad98cab72d8
BryanValc <epicspartanryan@gmail.com>
2026-09-26 01:58:39 +0000

    Mark Aseprite Wizard and Gut as dev so they're deleted on production build

--- client/sample-game/plug.gd
+++ client/sample-game/plug.gd
@@ -3,8 +3,10 @@ extends "res://addons/gd-plug/plug.gd"
 # More info: https://github.com/imjp94/gd-plug
 
 func _plugging():
+	# {"dev": true} can be excluded or uninstalled with "production" command
+
 	# Aseprite Wizard (Version v9.8.0-4)
-	plug("viniciusgerevini/godot-aseprite-wizard", {"tag": "v9.8.0-4"})
+	plug("viniciusgerevini/godot-aseprite-wizard", {"tag": "v9.8.0-4", "dev": true})
 	
 	# Unit test
-	plug("bitwes/Gut", {"tag": "v9.7.1"})
+	plug("bitwes/Gut", {"tag": "v9.7.1", "dev": true})
